dc82fe6600544ee3960d828349533cda1d5c0b67
Wagner Loch <wagner_loch@hotmail.com>
2025-03-26 11:13:48 -0300

    Add main pages

---
+++ app/(home)/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Página inicial!!</Text>
+      <Link href={"/login"}>Ir para o login</Link>
+      <Link href={"/register"}>Ir para o cadastro</Link>
+      <Link href={"/config"}>Ir para as configurações</Link>
+      <Link href={"/config/user"}>Ir para as configurações do usuário</Link>
+    </View>
+  );
+}
---
+++ app/(home)/register.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Register() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Esta é a página de cadastro!!!!</Text>
+    </View>
+  );
+}
